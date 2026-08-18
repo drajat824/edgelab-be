1df3d4ba5bd737e93c8a08b4b5c096dfc7e53665
drajat824 <drajat.fikri24@gmail.com>
2026-08-18 18:52:18 +0700

    tes

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://10.14.1.82:5173",
+        "http://192.168.1.8:5173",
         "http://raspijst.local:5173",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
