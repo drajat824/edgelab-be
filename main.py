57c499a1d54145116724d54985601f87d73b88cc
dajat824 <dajat.fikri24@gmail.com>
2026-08-19 19:20:52 +0700

    update

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ scripting_controller = DynamicScriptEngine(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://edgelab.local:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
