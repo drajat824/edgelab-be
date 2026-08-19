b49fdff81d82f2d2b61f4f8d5bca5931b968fc07
drajat824 <drajat.fikri24@gmail.com>
2026-08-19 20:44:58 +0700

    Update CORS middleware to allow localhost origin

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ scripting_controller = DynamicScriptEngine(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://edgelab.local:5173"],
+    allow_origins=["http://edgelab.local:5173", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
