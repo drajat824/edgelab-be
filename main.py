8e58cad27b6ac6f85f05e3fb6a9d8e420d9deea3
drajat824 <drajat.fikri24@gmail.com>
2026-08-19 17:00:58 +0700

    Update CORS middleware to allow all origins

--- main.py
+++ main.py
@@ -26,13 +26,7 @@ scripting_controller = DynamicScriptEngine(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://10.14.1.82:5173",
-        "http://192.168.1.8:5173",
-        "http://raspijst.local:5173",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
